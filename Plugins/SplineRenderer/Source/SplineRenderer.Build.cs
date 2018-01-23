77079dd891cddcf019a72dea2ff29b5e3bad1bd9
Oh-Hyun Kwon <kw@ucdavis.edu>
2018-01-23 12:33:52 -0800

    Update SplineRenderer.Build.cs

--- Plugins/SplineRenderer/Source/SplineRenderer.Build.cs
+++ Plugins/SplineRenderer/Source/SplineRenderer.Build.cs
@@ -1,38 +1,21 @@
-using System;
-using System.IO;
-
 namespace UnrealBuildTool.Rules
 {
     public class SplineRenderer : ModuleRules
     {
-        private string ThirdPartyPath
+        public SplineRenderer(ReadOnlyTargetRules Target) : base(Target)
         {
-            get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "../ThirdParty/")); }
-        }
-
-        public SplineRenderer(ReadOnlyTargetRules Target)
-        {
-            PublicIncludePaths.AddRange(new string[] {
-                });
-
             PrivateIncludePaths.AddRange(new string[] {
-                    "SplineRenderer/Private",
-                });
+                "SplineRenderer/Private",
+            });
 
             PublicDependencyModuleNames.AddRange(new string[] {
-                    "Core",
-					"CoreUObject",
-                    "Engine",
-                    "RenderCore",
-                    "ShaderCore",
-                    "RHI"
-                });
-
-            PrivateDependencyModuleNames.AddRange(new string[] {
-                });
-
-            DynamicallyLoadedModuleNames.AddRange(new string[] {
-                });
+                "Core",
+    			"CoreUObject",
+                "Engine",
+                "RenderCore",
+                "ShaderCore",
+                "RHI"
+            });
         }
     }
 }
